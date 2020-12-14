b243af75b69b468545fe009688acf7d90b02bb6b
liver man <theliverman@protonmail.com>
2020-12-14 08:24:00 -0800

    add text objects with rendering methods

--- PaperUtils.cs
+++ PaperUtils.cs
@@ -110,45 +110,80 @@ namespace PaperSDL {
     public class CenteredRectangle : CenteredObject {
         public Rectangle rectangle;
         public Vector2 size;
-
         public CenteredRectangle(Vector2 pos, Vector2 size) : base(pos){
             this.size = size;
             literalPosition = PaperUtils.CenterRectToPoint(pos, size);
 
             Center();
         }
-
         public Rectangle GetRectangle() {
             return rectangle;
         }
-
         public override void Center() {
             literalPosition = PaperUtils.CenterRectToPoint(position, size);
             rectangle = new Rectangle(literalPosition.X, literalPosition.Y, size.X, size.Y);
         }
-
         public override void Draw() {
             Raylib.DrawRectangleRec(rectangle, Color.BLACK); // Call this method if you want a custom colour
         }
     }
 
-    public class CenteredTexture : CenteredObject {
-        public Texture2D texture;
+    public class TextObject : CenteredObject {
+        public string text = "";
+        public FontData fontData;
+        public Color color;
 
-        public CenteredTexture(Vector2 pos, Texture2D texture) : base(pos) {
-            this.texture = texture;
+        public TextObject(Vector2 pos, FontData fd, Color color) : base(pos) {
+            this.position = pos;
+            this.fontData = fd;
+            this.color = color;
 
             Center();
         }
 
-        public Texture2D GetTexture() {
-            return texture;
+        public TextObject(Vector2 pos, FontData fontData, Color color, string text) : base(pos) {
+            this.position = pos;
+            this.fontData = fontData;
+            this.color = color;
+            this.text = text;
+
+            Center();
+        }
+
+        public void SetText(string newText) {
+            this.text = newText;
+            Center();
         }
 
         public override void Center() {
+            literalPosition = position;
+        }
+
+        public override void Draw() {
+            PaperUtils.DrawText(fontData, text, literalPosition, color);
+        }
+    }
+
+    public class CenteredText : TextObject {
+        public CenteredText(Vector2 pos, FontData fd, Color color) : base(pos, fd, color) {}
+        public CenteredText (Vector2 pos, FontData fontData, Color color, string text) : base(pos, fontData, color, text) {}
+        public override void Center() {
+            literalPosition = PaperUtils.CenterTextToPoint(position, fontData, text);
+        }
+    }
+
+    public class CenteredTexture : CenteredObject {
+        public Texture2D texture;
+        public CenteredTexture(Vector2 pos, Texture2D texture) : base(pos) {
+            this.texture = texture;
+            Center();
+        }
+        public Texture2D GetTexture() {
+            return texture;
+        }
+        public override void Center() {
             literalPosition = PaperUtils.CenterTextureToPoint(position, texture);
         }
-        
         public override void Draw() {
             Raylib.DrawTexture(texture, (int)literalPosition.X, (int)literalPosition.Y, Color.WHITE);
         }
--- SampleProgram.cs
+++ SampleProgram.cs
@@ -9,6 +9,7 @@ public class SampleProgram : PaperApp {
     CenteredRectangle rect;
     // Texture2D texture;
     FontData defaultFont;
+    TextObject centeredText;
 
     public static void Main() {
         new SampleProgram();
@@ -17,22 +18,21 @@ public class SampleProgram : PaperApp {
     public override void Start() {
         defaultFont = new FontData(Raylib.GetFontDefault(), 16);
         rect = new CenteredRectangle(new Vector2(width/2, height/2), new Vector2(100, 100));
+
+        // CenteredText Showcase
+        centeredText = new CenteredText(new Vector2(width/2, height/2), new FontData(Raylib.GetFontDefault(), 16f), Color.WHITE, "PaperSDL");
     }
 
     public override void Update() {
         if(PaperUtils.RectClicked(rect.GetRectangle(), MouseButton.MOUSE_LEFT_BUTTON)) {
             col = (col.Equals(Color.GREEN)) ? Color.RED : Color.GREEN;
+            centeredText.SetText(centeredText.text.Equals("PaperSDL") ? "Built on Raylib" : "PaperSDL");
         }
     }
 
     public override void Draw() {
         Raylib.ClearBackground(Color.DARKGRAY);
         Raylib.DrawRectangleRec(rect.GetRectangle(), col);
-    }
-
-    private void CenterWindow() {
-        // Raylib.SetWindowPosition(Raylib.GetMonitorWidth(0)/2 - Raylib.GetScreenWidth()/2, Raylib.GetMonitorHeight(0)/2 );
-        Raylib.SetWindowPosition(0,0);
-        
+        centeredText.Draw();
     }
 }
